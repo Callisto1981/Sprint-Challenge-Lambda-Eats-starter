a269f8ce4ffb8951bd6850803a2454c05261ae34
Wayne <wabbruscatoorr@gmail.com>
2020-03-27 12:53:26 -0400

    updating form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,103 +8,178 @@ const formSchema = yup.object().shape({
   name: yup
     .string()
     .required("Name is Required")
-    .min(2, "Name must be 2 characters or more")
+    .min(2, "Name must be 2 characters or more"),
+  size: yup.string().required("Select a size"),
+  pepperoni: yup.boolean().oneOf([true]),
+  onions: yup.boolean().oneOf([true]),
+  peppers: yup.boolean().oneOf([true]),
+  tomatoes: yup.boolean().oneOf([true])
 });
 
 const Form = () => {
   const [button, setButton] = useState(true);
 
-
   const [formState, setFormState] = useState({
-      name: ""
+    name: "",
+    size: "",
+    pepperoni: "",
+    onions: "",
+    peppers: "",
+    tomatoes: ""
   });
-
 
   const [errors, setErrors] = useState({
-      name:""
+    name: "",
+    size: "",
+    pepperoni: "",
+    onions: "",
+    peppers: "",
+    tomatoes: ""
   });
 
-  const[post, setPost] = useState([]);
+  const [post, setPost] = useState([]);
 
-  useEffect(()=> {
-      formSchema.isValid(formState).then(valid => {
-          setButton(!valid);
-      });
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButton(!valid);
+    });
   }, [formState]);
 
-
   const formSubmit = e => {
-      e.preventDefault();
-      axios.post("https://reqres.in/api/pizza", formState)
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/pizza", formState)
       .then(response => {
-          setPost(response.data);
+        setPost(response.data);
 
-          setFormState({
-              name:""
-          });
+        setFormState({
+          name: "",
+          size: "",
+          pepperoni: "",
+          onions: "",
+          peppers: "",
+          tomatoes: ""
+        });
       })
       .catch(error => console.log("something is wrong", error.response));
   };
 
   const validateChange = e => {
-      yup.reach(formSchema, e.target.name)
-      validate(e.target.name === "terms" ? e.target.checked : e.target.value)
+    yup.reach(formSchema, e.target.name);
+    validate(e.target.name === "terms" ? e.target.checked : e.target.value)
       .then(valid => {
-          setErrors({
-              ...errors, [e.target.name]: ""
-          });
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
       })
       .catch(error => {
-          setErrors({
-              ...errors, [e.target.name]: error.errors[0]
-          });
+        setErrors({
+          ...errors,
+          [e.target.name]: error.errors[0]
+        });
       });
   };
 
-
   const inputChange = e => {
-      e.persist();
-      const newFormData = {
-          ...formState,
-          [e.target.name]: e.target.type === "checkbox" ? e,target.checked : e.target.value
-      };
-      validateChange(e);
-      setFormState(newFormData);
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value
+    };
+    validateChange(e);
+    setFormState(newFormData);
   };
 
   return (
     <form onSubmit={formSubmit}>
-        <Link to={"/"}>
-            <div>Home</div>
-        </Link>
+      <Link to={"/"}>
+        <div>Home</div>
+      </Link>
       <label htmlFor="name">
         Name:
-        <input id="name" type="text" name="name" value={formState.name} onChange={inputChange} />
-      {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
-      </label><br/>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={inputChange}
+        />
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+      </label>
+      <br />
 
       <label htmlFor="size">
-          What size Pizza: 
-        <select 
-        id="size"
-        name="name"
-        onChange={inputChange}>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
+        What size Pizza:
+        <select id="size" name="name" onChange={inputChange}>
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
         </select>
-        </label><br/>
+      </label>
+      <br />
 
-        <label htmlFor="pepperoni">
-            pepperoni
-            <input 
-            id="pepperoni"
-            type="checkbox"
-            name="pepperoni"
-            checked={formState.pepperoni}
-            onChange={inputChange}/>
-            {errors.pepperoni.length > 0 ? <p className="pepperoni">{errors.pepperoni}</p> : null}
-        </label>
+      <label htmlFor="pepperoni">
+        pepperoni
+        <input
+          id="pepperoni"
+          type="checkbox"
+          name="pepperoni"
+          checked={formState.pepperoni}
+          onChange={inputChange}
+        />
+        {errors.pepperoni.length > 0 ? (
+          <p className="pepperoni">{errors.pepperoni}</p>
+        ) : null}
+      </label>
+      <br />
+
+      <label htmlFor="onions">
+        onions
+        <input
+          id="onions"
+          type="checkbox"
+          name="onions"
+          checked={formState.onions}
+          onChange={inputChange}
+        />
+        {errors.onions.length > 0 ? (
+          <p className="onions">{errors.onions}</p>
+        ) : null}
+      </label>
+      <br />
+
+      <label htmlFor="peppers">
+        peppers
+        <input
+          id="peppers"
+          type="checkbox"
+          name="peppers"
+          checked={formState.peppers}
+          onChange={inputChange}
+        />
+        {errors.peppers.length > 0 ? (
+          <p className="peppers">{errors.peppers}</p>
+        ) : null}
+      </label>
+      <br />
+
+      <label htmlFor="tomatoes">
+        tomatoes
+        <input
+          id="tomatoes"
+          type="checkbox"
+          name="tomatoes"
+          checked={formState.tomatoes}
+          onChange={inputChange}
+        />
+        {errors.tomatoes.length > 0 ? (
+          <p className="tomatoes">{errors.tomatoes}</p>
+        ) : null}
+      </label>
+      <br />
+
       <pre>{JSON.stringify(post, null, 2)}</pre>
       <button disabled={button}>Submit</button>
     </form>
